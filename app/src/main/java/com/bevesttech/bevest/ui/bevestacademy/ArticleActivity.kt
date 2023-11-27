e3c40e0aba002c882a722d8f419e01242dc4fd76
Hakim <hakim.vengeance155@gmail.com>
2023-11-27 11:53:35 +0700

    finishing articleactivity

--- app/src/main/java/com/bevesttech/bevest/ui/bevestacademy/ArticleActivity.kt
+++ app/src/main/java/com/bevesttech/bevest/ui/bevestacademy/ArticleActivity.kt
@@ -3,10 +3,24 @@ package com.bevesttech.bevest.ui.bevestacademy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bevesttech.bevest.R
+import com.bevesttech.bevest.databinding.ActivityArticleBinding
 
 class ArticleActivity : AppCompatActivity() {
+    private var _binding: ActivityArticleBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_article)
+        _binding = ActivityArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppbar.setNavigationOnClickListener {
+            finish()
+        }
     }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
 }
--- app/src/main/java/com/bevesttech/bevest/ui/valuasi/ValuasiFailedFragment.kt
+++ app/src/main/java/com/bevesttech/bevest/ui/valuasi/ValuasiFailedFragment.kt
@@ -1,5 +1,6 @@
 package com.bevesttech.bevest.ui.valuasi
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,8 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.bevesttech.bevest.R
 import com.bevesttech.bevest.databinding.FragmentValuasiFailedBinding
+import com.bevesttech.bevest.ui.bevestacademy.ArticleActivity
 
-class ValuasiFailedFragment : Fragment() {
+class ValuasiFailedFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentValuasiFailedBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -21,6 +23,26 @@ class ValuasiFailedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            btnAjukanKembali.setOnClickListener(this@ValuasiFailedFragment)
+            btnBevestAcademy.setOnClickListener(this@ValuasiFailedFragment)
+
+            }
+        }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_ajukan_kembali -> {
+
+            }
+
+            R.id.btn_bevest_academy -> {
+                Intent(requireActivity(), ArticleActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
@@ -28,3 +50,4 @@ class ValuasiFailedFragment : Fragment() {
         super.onDestroy()
     }
 }
+
